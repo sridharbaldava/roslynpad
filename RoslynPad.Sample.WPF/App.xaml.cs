8a9379b8f77a53dc8b5f6ea9d334e1ec21fe7c60
Sridhar Baldava <SBaldava@walterpmoore.com>
2020-01-27 08:38:05 -0600

    sample added

---
+++ RoslynPad.Sample.WPF/App.xaml.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.CodeAnalysis.CodeActions;
+using RoslynPad.Roslyn.CodeActions;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace RoslynPad.Sample.WPF
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+    internal sealed class CodeActionsConverter : MarkupExtension, IValueConverter
+    {
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return this;
+        }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return ((CodeAction)value).GetCodeActions();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
